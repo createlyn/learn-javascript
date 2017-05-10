29f28616d5100e6c7f44b222cde0eabd4fefbde8
createlyn <caitlyn.zahn@gmail.com>
2017-05-10 16:36:25 -0400

    completed section 2.2

--- js/basics.js
+++ js/basics.js
@@ -3,3 +3,35 @@ var theMeaning OfLife = 42;
   baz, quux;
 
 foo = baz;
+
+// strings
+var number = 1;
+var string= 'hello';
+var quotes = '"Hello", said the man';
+var escaped = '"Oh no you didn\'t" she said';
+var joined = 'wait, ' + 'what?';
+
+// booleans = true or false
+
+
+// array []
+// container for multiple values
+// fixed numerical indices
+// indices are zero-bases
+
+var array = [5, 'something', true];
+//array[0]; - will return = 5
+
+
+// objects
+// container for key value pairs
+// pairs separated by commas
+// Key and value separated by colon
+
+var object = {
+  myNumber: 10,
+  myString: 'woohoo!'
+};
+
+object.myNumber; // will return 10
+object['myString']; // will return 'woohoo!'
