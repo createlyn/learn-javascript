f239224f752941833366997d47b833eb7134eb27
createlyn <caitlyn.zahn@gmail.com>
2017-05-10 19:48:09 -0400

    added comparison operators

--- js/basics.js
+++ js/basics.js
@@ -1,8 +1,8 @@
-var theMeaning OfLife = 42;
-  foo = 'bar',
-  baz, quux;
+var theMeaningOfLife = 42;
+  // foo = 'bar',
+  // baz, quux;
 
-foo = baz;
+// foo = baz;
 
 // strings
 var number = 1;
@@ -35,3 +35,54 @@ var object = {
 
 object.myNumber; // will return 10
 object['myString']; // will return 'woohoo!'
+
+
+// operators
+var addition = 1 + 1; // addition = 2
+var subtraction = 1 - 1; // subtraction = 0
+var multiplication = 5 * 2; // multiplication = 10
+var division = 5 / 2.5; // division = 2
+var remainder = 5 % 2 // remainder = 1
+
+
+var postfix = 5;
+var prefix = 5;
+
+var additionAssignment = 1;
+additionAssignment +- 1; // additionAssignment = 2
+
+var subtractionAssignment = 1;
+subtractionAssignment -= 1; // subtractionAssignment = 0
+
+var multiplicationAssignment = 2;
+var divisionAssignment = 5;
+var modulusAssignment = 5;
+multiplicationAssignment *= 2 // multiplicationAssignment = 4
+divisionAssignment /= 2; // divisionAssignment = 2
+modulusAssignment %= 2; // modulusAssignment = 1
+
+// comparison operators
+// equality comparison
+// greater-than comparison
+// less-than comparison
+
+// regular/non-strict ==
+// strict ===
+
+// 1 == true -- '1 is equal to true' -- returns true
+// 1 === true -- '' -- returns false
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
