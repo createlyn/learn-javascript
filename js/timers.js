4a4c09fa14f50b8acb9ffb449f0a34ff0d68dea0
createlyn <caitlyn.zahn@gmail.com>
2017-05-16 13:56:23 -0400

    finished section 3

--- js/timers.js
+++ js/timers.js
@@ -3,6 +3,25 @@
 (function () {
   'use strict';
 
-  setTimeout('console.log("bad!")', 1000);
+  // on setTimeout NEVER pass string as argument
+  // setTimeout('console.log("bad!")', 1000); - bad practice
+
+  function log(message) {
+    console.log(message);
+  }
+  setTimeout(function() {
+    log('woo!');
+  }, 1000);
+
+  function pulse() {
+    console.log('boom, boom');
+    counter += 1;
+
+    if (counter === 5) {
+      clearInterval(interval);
+    }
+  }
+  var interval = setInterval(pulse, 1000),
+    counter = 0;
 
 }());
