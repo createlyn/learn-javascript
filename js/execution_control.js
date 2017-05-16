8f3cefca564d25faccb46f870c16b43ae577729a
createlyn <caitlyn.zahn@gmail.com>
2017-05-16 13:20:40 -0400

    finished 3.4 loops

--- js/execution_control.js
+++ js/execution_control.js
@@ -1,4 +1,7 @@
 // this branch basics.js was commented out
+
+
+// if then statements
 (function() {
   'use strict';
 
@@ -30,8 +33,12 @@
     console.log(array[0]);
   }
 
+  // shorthand
+
   (true) ? console.log('This one') : console.log('Not this one');
 
+
+  // switch statements
   var switcher = 'two';
   switch (switcher) {
     case 'one':
@@ -53,6 +60,53 @@
     default:
       console.log('switcher')
   }
+
+  // loops
+  // execute a block of statements multiple times
+  // 4 different kinds
+  // the more times the loop executes, the longer it takes to run!
+
+  // for - used with arrays
+  var anArray = ['item1', 'item2', 'item3'];
+  for (var x = 0, y = anArray.length; x < y; x+=1) {
+    console.log(anArray[x]);
+  }
+
+  // for in - used with objects
+  var anObject = {
+    prop1: 'property1',
+    prop2: 'property2',
+    prop3: 'property3'
+  };
+  for (var prop in anObject) {
+    if (anObject.hasOwnProperty(prop)) {
+      console.log(anObject[prop]);
+    }
+  }
+
+
+  // while -
+  var limit = 0;
+  while (limit < 5) {
+    if (limit === 3) {
+      break;
+    }
+    console.log(limit += 1);
+  }
+
+
+  // do while - execute one more more statements while a condition returns true
+  // statements will always be executed at least once
+  // break is used to exit the loop early
+
+  var condition = true;
+  do {
+    console.log('once');
+    break;
+  } while (condition);
+
+
+
 
 }());
 
